2a9af6581852ff8639ad806a002b98239d3c7698
Dave Paul Joseph <davepauljoseph@gmail.com>
2023-12-15 20:54:34 +0530

    Navbar and subsequent changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Billboard from './Components/Billboard';
-import Header from './Components/Header';
+import NavCustom from './Components/Navbars';
 import Footer from './Components/Footer';
-import Prop from './Components/Prop';
 import Sm from './Components/Socialmedia';
 import Oppam from './Components/Oppam';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Styles/App.css';
 
 function App() {
   return (
     <div className="App" style={{ backgroundColor: '#e4c0cb', maxWidth: '100vw' }}>
-      <Prop />
-      <Header />
+        <div id="top" style={{ height: 0 }}/>
+        <NavCustom className="sticky-nav"></NavCustom>
       <main>
         <Router>
           <Routes>
---
+++ src/Components/Navbars.jsx
@@ -0,0 +1,35 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar'
+import { Image } from 'react-bootstrap';
+import '../Styles/Navbars.css';
+import { Link } from 'react-scroll';
+function NavCustom() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary sticky-top" id="navbar">
+      <Container fluid>
+        <div className='props'> 
+        <Image src='./eclogoblack.png' width={60} height={60}></Image>
+        <div className='titlebox'>
+        <div className='title'>ECE BillBoard</div>
+        <div className='smalltitle'>College of Engineering Trivandrum</div>
+        </div>
+        </div>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <div className='spacer'></div>
+          <Nav className="mx-auto">
+            <Nav.Link><Link to="top" smooth= {true} duration={500}>Home</Link></Nav.Link>
+            <Nav.Link><Link to="prop" smooth= {true} duration={500}>Gallery</Link></Nav.Link>
+            <Nav.Link><Link to="billboard" smooth= {true} duration={500}>Notifications</Link></Nav.Link>
+          </Nav>
+          <Nav className='mr-auto'>
+          <Nav.Link href="#contacts"><Link to="sm" smooth= {true} duration={500}>Contact us</Link></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavCustom;
