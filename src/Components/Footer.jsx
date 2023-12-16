a4f6adbce3c0ec7918a71f22faacdae8c4ce94c1
Dave Paul Joseph <davepauljoseph@gmail.com>
2023-12-16 07:44:14 +0530

    Footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
-import '../Styles/Footer.css'
-
-const Footer = () => {
-    return (
-        <footer className="footer">
-            <p>ECE BILLBOARD &copy; 2023</p>
-        </footer>
-    );
-};
-
+import { MDBFooter, MDBContainer, MDBCol, MDBRow, MDBIcon } from 'mdb-react-ui-kit';
+import '../Styles/Footer.css';
+function Footer() {
+  return (
+    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+      <MDBContainer className='p-4'>
+      <MDBRow className='mt-3'>
+      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+      <MDBCol size="auto">
+      <div className='webteam'>
+        <span>The web team:</span>
+        <a href='h1' className='me-4 text-reset'>
+            <MDBIcon color='grey' fab icon='facebook-f' />
+          </a>
+        </div>
+      </MDBCol>
+      <MDBCol size="auto">
+        <div>
+          <a href='h1' className='me-4 text-reset'>
+            <MDBIcon color='grey' fab icon='facebook-f' />
+          </a>
+          <a href='h2' className='me-4 text-reset'>
+            <MDBIcon color='grey' fab icon='twitter' />
+          </a>
+          <a href='h3' className='me-4 text-reset'>
+            <MDBIcon color='grey' fab icon='google' />
+          </a>
+          <a href='h4' className='me-4 text-reset'>
+            <MDBIcon color='grey' fab icon='instagram' />
+          </a>
+          <a href='h5' className='me-4 text-reset'>
+            <MDBIcon color='grey' fab icon='linkedin' />
+          </a>
+          <a href='h6' className='me-4 text-reset'>
+            <MDBIcon color='grey' fab icon='github' />
+          </a>
+        </div>
+        </MDBCol>
+      </section>
+      </MDBRow>
+      </MDBContainer>
+      
+    </MDBFooter>
+  );
+}
 export default Footer;
