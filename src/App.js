b93e3cd834fa47d19b97574b2190322e2b2758eb
Dave Paul Joseph <davepauljoseph@gmail.com>
2024-02-19 13:40:30 +0530

    billoboard

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ import './Styles/App.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "mdbreact/dist/css/mdb.css";
 import CarouselCustom from './Components/Carousal';
+import Billboard from './Components/Billboard';
 import LoadingScreen from './Components/Loading';
 function App() {
   const [carousalItems, setCarousal] = useState([]);
   const [galleryItems, setGallery] = useState([]);
+  const [billboardItems, setBillboard] = useState([]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     var base = new Airtable({apiKey: 'patEIftf6ErouVFwc.f3085100c905da7b0bf5336990947424495795a0b4b0c4ec735384678ca7021e'}).base('app3s7iPWjKOvxwVy');
@@ -54,6 +56,24 @@ function App() {
       setLoading(false);
       if (err) { console.error(err); return; }
   });
+  base('Billboard').select({
+    maxRecords: 10,
+    view: "Grid view"
+}).eachPage(function page(galleryItems, fetchNextPage) {
+    const updatedGalleryItems = [...galleryItems, ...galleryItems.map(record => ({
+      name: record.get('name'),
+      description: record.get('description'),
+      tag: record.get('tag'),
+      imageUrl: record.get('imageUrl')[0].url
+    }))];
+
+    setGallery(updatedGalleryItems);
+    fetchNextPage();
+
+}, function done(err) {
+    setLoading(false);
+    if (err) { console.error(err); return; }
+});
   }, []);
   return (
     <div>
@@ -64,14 +84,17 @@ function App() {
     ) : (
       <div className="App" style={{ backgroundColor: 'black', maxWidth: '100vw' }}>
       <div id="top" style={{ height: 0 }}/>
-      <NavCustom className="sticky-nav"></NavCustom>
-      <CarouselCustom records={carousalItems}/>   
+      <NavCustom className="sticky-nav"></NavCustom> 
     <main>
-    
       <Router>
         <Routes>
-          <Route path="/cards" element={<div className="title">Cards</div>} />
-          <Route path="/" element={<CardHolder records={galleryItems}/>} />
+          <Route path="/" element={
+          <div className='wrapper_home'>
+          <CarouselCustom records={carousalItems}/> 
+          <CardHolder records={galleryItems}/>
+          <Billboard records={billboardItems}></Billboard>
+          </div>
+          } />
         </Routes>
       </Router>
     </main>
--- src/Components/Billboard.jsx
+++ src/Components/Billboard.jsx
@@ -1,41 +1,9 @@
-import React, { useEffect, useState } from 'react';
 import '../Styles/Billboard.css';
-import Airtable from 'airtable';
-import Card from './Card';
-function Billboard() {
-    const [records, setRecords] = useState([]);
-    useEffect(() => {
-      var base = new Airtable({apiKey: 'patEIftf6ErouVFwc.f3085100c905da7b0bf5336990947424495795a0b4b0c4ec735384678ca7021e'}).base('app3s7iPWjKOvxwVy');
-  
-      base('Events').select({
-          maxRecords: 10,
-          view: "Grid view"
-      }).eachPage(function page(records, fetchNextPage) {
-          const updatedRecords = [...records, ...records.map(record => ({
-            name: record.get('Name'),
-            regLink: record.get('Registration link'),
-            assets: record.get('Assets')[0].url,
-            eventDate: record.get('Event Date'),
-            tags: record.get('Tags'),
-            deadline: record.get('Regdeadline'),
-            contact: record.get('Contact info'),
-            description: record.get('Description')
-          }))];
-          console.log(updatedRecords);
-          setRecords(updatedRecords);
-          fetchNextPage();
-  
-      }, function done(err) {
-          if (err) { console.error(err); return; }
-      });
-    }, []);
+function Billboard(props) {
     return (
         <div className="billboard-container" id='billboard'>
-            <Card name="test" description="test2" url="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F1.bp.blogspot.com%2F-kB9LF7kd6_U%2FTmRLtKn7oHI%2FAAAAAAAAA2c%2F4f9PfPb0yJY%2Fs1600%2Frat_1.jpg&f=1&nofb=1&ipt=519bc490036b96ac84df2ac0e0c6c796aa6c993ffc14e6b42cdebce73b959c3f&ipo=images"></Card>
+            fskdljflj
         </div>
-        
-
-        
     );
 }
 
