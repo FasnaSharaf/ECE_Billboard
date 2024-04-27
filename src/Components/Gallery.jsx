013ff8682557ee5179d77c847b8f6006c20a365b
Abhi-m-anue <125118501+Abhi-m-anue@users.noreply.github.com>
2024-04-27 20:22:10 +0530

    testing

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -14,7 +14,7 @@ function Gallery({records}) {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 2,
+    slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
       {
@@ -35,7 +35,7 @@ function Gallery({records}) {
         }
       },
       {
-        breakpoint: 680,
+        breakpoint: 600,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
@@ -45,42 +45,45 @@ function Gallery({records}) {
   };
   return (
     <div style={{
-      width:"80vh", margin: "0 auto"
-    }} >
-      <Slider {...settings}>
-    
+      width: "80vw", margin: "0 auto"
+    }} className="outer-box">
+      <div className="section_cards gallery">
+    <div className="title_cards"><h1>GALLERY</h1></div>
+    <div className="cards">
+    <ul className="card_holder">
     {records.map(record => (
-      // Check if imageUrl is valid before rendering Carousel.Item
-      record.imageUrl && (
-        <div key={record.name} style = {{height: "450px"}}>
-          <div >
+        // Check if imageUrl is valid before rendering Carousel.Item
+        record.imageUrl && (
           <Card url={record.imageUrl} name={record.name} tag={record.tag} description={record.description}></Card>
-          </div>
-
-          
-        </div>
+          )
+      ))}
+     
+    </ul>
+    </div>
+    </div>
+      <div style={{marginTop: "20px", maxWidth:"90vw"}} className="hide">
+      <div className="title_cards"><h1>GALLERY</h1></div>
+        <Slider {...settings}>
+            
+            {records.map(record => (
+        // Check if imageUrl is valid before rendering Carousel.Item
+        record.imageUrl && (
+          <div key={record.name} style = {{height: "450px"}}>
+            <div >
+            <Card url={record.imageUrl} name={record.name} tag={record.tag} description={record.description}></Card>
+            </div>
         
-        )
-    ))}
-    </Slider>
+          </div>
+        
+          )
+            ))}
+            </Slider>
+      </div>
       
       
     </div>
     
-    // <div className="section_cards gallery">
-    // <div className="title_cards"><h1>GALLERY</h1></div>
-    // <div className="cards">
-    // <ul className="card_holder">
-    // {records.map(record => (
-    //     // Check if imageUrl is valid before rendering Carousel.Item
-    //     record.imageUrl && (
-    //       <Card url={record.imageUrl} name={record.name} tag={record.tag} description={record.description}></Card>
-    //       )
-    //   ))}
-     
-    // </ul>
-    // </div>
-    // </div>
+    
   );
 }
 
