c52af6db0dac14cd53a2ddf5dffd5599f6eda4d7
Abhi-m-anue <125118501+Abhi-m-anue@users.noreply.github.com>
2024-05-06 12:17:54 +0530

    undo changes

--- src/Components/Billboard.jsx
+++ src/Components/Billboard.jsx
@@ -81,7 +81,7 @@ const Billboard = () => {
                   >
                     <div className="bill_desc">{record.description}</div>
                     <div className="bill_date">{record.date}</div>
-                    <div className = "temp" style={{ cursor: "pointer", padding: "2.1vh" }}>
+                    <div style={{ cursor: "pointer", padding: "2.1vh" }}>
                       {index === note ? (
                         <FaChevronUp />
                       ) : (
